6db1f1fe34bc9fdc06d87e3dae7c28e1c8a63cf5
Fran Simó <fransimo@gmail.com>
2025-06-20 15:25:14 +0300

    ipns

---
+++ update_ipns.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+curl https://fransimo.info/cid.txt -o new_cid.txt
+
+new_cid = ` cat new_cid.txt `
+old_cid = ` cat old_cid.txt `
+
+# Compare the files
+if ! cmp -s "$new_cid" "$old_cid"; then
+    echo "CID has changed."
+    docker exec ipfs_host ipfs name publish --key=k51qzi5uqu5dhiu7v9v01yb6i69yu4luny793hwl19knqbqdgv97x7s949h0h1 --lifetime 48h --ttl 48h $new_cid
+    eval "$ON_CHANGE_COMMAND"
+else
+    echo "CID has not changed."
+fi
