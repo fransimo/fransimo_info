227eb1f696f67e3f642a5c6c2f6fae6fd7701bd3
Fran Simó <fransimo@gmail.com>
2025-08-22 16:31:32 +0300

    PDFs CVs

--- content/docs/cv/update_pdf_cv.sh
+++ content/docs/cv/update_pdf_cv.sh
@@ -1,10 +1,24 @@
-sed '1{/^---$/!q;};1,/^---$/d' detailed_cv.es.md > tmp.md
+function convert_2_pdf() {
 
-pandoc tmp.md -t docx -o tmp.docx
+  sed '1{/^---$/!q;};1,/^---$/d' $1 > tmp.md
 
-pandoc tmp.md  -f gfm -t pdf -o tmp.pdf  \
-      --include-in-header pdf_prop.tex \
-      -V geometry:"portrait, margin=2cm, a4paper" \
-      -V mainfont="DejaVu Serif" \
-      -V monofont="DejaVu Sans Mono" \
-      -V fontsize=12pt ## --toc
+  pandoc tmp.md -t docx -o tmp.docx
+
+  rm $2
+
+  pandoc tmp.md  -f gfm -t pdf -o $2  \
+        --include-in-header pdf_prop.tex \
+        -V geometry:portrait \
+        -V geometry:top=2cm \
+        -V geometry:right=2cm \
+        -V geometry:bottom=2cm \
+        -V geometry:left=2cm \
+        -V geometry:a4paper \
+        -V mainfont="DejaVu Serif" \
+        -V monofont="DejaVu Sans Mono" \
+        -V fontsize=12pt ## --toc
+
+  rm tmp.md
+}
+
+convert_2_pdf "detailed_cv.es.md" "detailed_cv.es.pdf"
