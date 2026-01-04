3b20af82aead4c346b357d03484c94093740c281
Fran Simó <fransimo@gmail.com>
2026-01-04 17:04:16 +0300

    generate ipfs

--- generate_ipfs.sh
+++ generate_ipfs.sh
@@ -2,6 +2,7 @@
 ## generation
 rm -fR public/
 
+
 docker run --rm -it \
   -v $(pwd):/src \
   -p 1313:1313 \
@@ -13,15 +14,20 @@ if [[ $rc != 0 ]]; then
   echo Error during site generation
   exit $rc
 fi
+
 ## CAR
-npx ipfs-car pack public --output build.car
+if [ -f "build.car" ] ; then
+  rm build.car
+fi
+
+npx ipfs-car pack public --output build.car > new_cid.txt
 rc=$?
 if [[ $rc != 0 ]]; then
   echo Error during CAR generation
   exit $rc
 fi
 
-npx ipfs-car hash build.car > new_cid.txt
+npx ipfs-car roots build.car > new_cid.txt
 
 ## ipfs publish
 
@@ -29,9 +35,13 @@ new_cid=` cat new_cid.txt `
 old_cid=` cat old_cid.txt `
 old_delete_cid=` cat old_delete_cid `
 
+if [ -f "~/ipfs/staging/build.car" ] ; then
+  rm ~/ipfs/staging/build.car
+fi
+
 mv build.car ~/ipfs/staging/
 
-docker exec ipfs_host ipfs dag import /export/build.car
+docker exec ipfs_host ipfs dag import /export/build.car > cid.txt
 docker exec ipfs_host ipfs name publish --key=k51qzi5uqu5dhiu7v9v01yb6i69yu4luny793hwl19knqbqdgv97x7s949h0h1 --lifetime 48h --ttl 48h $new_cid
 docker exec ipfs_host ipfs pin rm $old_delete_cid
 
