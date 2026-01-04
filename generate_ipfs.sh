f6a3093d25885ce47ea257702b5f3a8b33498baa
Fran Simó <fransimo@gmail.com>
2026-01-04 16:45:49 +0300

    generate ipfs

--- generate_ipfs.sh
+++ generate_ipfs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+## generation
 rm -fR public/
 
 docker run --rm -it \
@@ -13,7 +13,7 @@ if [[ $rc != 0 ]]; then
   echo Error during site generation
   exit $rc
 fi
-
+## CAR
 npx ipfs-car pack public --output build.car
 rc=$?
 if [[ $rc != 0 ]]; then
@@ -23,15 +23,18 @@ fi
 
 npx ipfs-car hash build.car > new_cid.txt
 
+## ipfs publish
+
 new_cid=` cat new_cid.txt `
 old_cid=` cat old_cid.txt `
 old_delete_cid=` cat old_delete_cid `
 
-mv /tmp/build.car ~/ipfs/staging/
+mv build.car ~/ipfs/staging/
 
 docker exec ipfs_host ipfs dag import /export/build.car
 docker exec ipfs_host ipfs name publish --key=k51qzi5uqu5dhiu7v9v01yb6i69yu4luny793hwl19knqbqdgv97x7s949h0h1 --lifetime 48h --ttl 48h $new_cid
 docker exec ipfs_host ipfs pin rm $old_delete_cid
+
 
 cp old_cid.txt old_delete_cid.txt
 cp new_cid.txt old_cid.txt
